df392374ecd2f6467ab98116ec8b35e84e54f951
Juan Ignacio Guastello <jiguastello@hotmail.com>
2020-11-04 19:19:21 -0300

    add app.js, folders, html

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express;
+const path = require('path');
+
+//app.use(express.static('public'));
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+/*app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/product', (req,res)=>{
+    res.sendFile(__dirname + '/views/product.html');
+});
+
+app.get('/checkout', (req,res)=>{
+    res.sendFile(__dirname + '/views/checkout.html');
+});
+*/

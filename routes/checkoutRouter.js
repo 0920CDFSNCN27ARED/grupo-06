5d4abfbc764446ab9aaa72cbc48eacc7b9309b1c
Juan Ignacio Guastello <jiguastello@hotmail.com>
2020-12-03 17:01:56 -0300

    corrección de errores

--- routes/checkoutRouter.js
+++ routes/checkoutRouter.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const checkoutController = require("../controllers/checkoutController");
 
-
-
-router.get("/checkout", checkoutController.checkout);
+router.get("/", checkoutController.checkout);
 
 module.exports = router;

43340a38a35bbdc20680e004c2696c551c97dfd8
Juan Ignacio Guastello <jiguastello@hotmail.com>
2020-12-03 20:16:09 -0300

    solución render en indexController

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,8 @@
 const path = require("path");
 
 const controller = {
-    /*index: (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../views/index.html"));
-    }*/
     index: (req, res) => {
-        res.render(path.resolve(__dirname, "index.ejs"));
+        res.render("index");
     },
 };
 

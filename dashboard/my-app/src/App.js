cf168be41d89c20c4c3cbfb87c786478b339b091
Juan Ignacio Guastello <jiguastello@hotmail.com>
2021-03-08 19:32:01 -0300

    react components

--- dashboard/my-app/src/App.js
+++ dashboard/my-app/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import SideMenu from './components/side-menu/SideMenu';
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div className="App">
       <div id="wrapper">
         <SideMenu />
+        <div id="content-wrapper" class="d-flex flex-column"> 
+              <div id="content">
+                <Header /> 
+              </div>
+        </div>
         </div> 
     </div>
   );

79c00d335fbf437dae91ad5c4693e16bd6df107d
Juan Ignacio Guastello <jiguastello@hotmail.com>
2021-03-08 01:15:30 -0300

    comenzado con react

---
+++ dashboard/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SideMenu from './components/side-menu/SideMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="wrapper">
+        <SideMenu />
+        </div> 
+    </div>
+  );
+}
+
+export default App;

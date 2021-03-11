742cfa082331354659ab7d3d24f7fecaa5b9324c
SilvestreNovaro <silvestrenovarohueyo@gmail.com>
2021-03-11 18:21:13 -0300

    ultimo commit silvestre

--- site/routes/productRouter.js
+++ site/routes/productRouter.js
@@ -9,7 +9,7 @@ const { check, validationResult, body } = require("express-validator");
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "site/public/images/products");
+        cb(null, "public/images/products");
     },
     filename: function (req, file, cb) {
         cb(
@@ -39,7 +39,7 @@ router.post(
         check("name")
             .isLength({ min: 5 })
             .withMessage("Debe tener más de 5 caracteres"),
-        check("name").isEmpty().withMessage("No debe estar vacio"),
+        //check("name").isEmpty().withMessage("No debe estar vacio"),
         check("description")
             .isLength({ min: 20 })
             .withMessage("Debe tener más de 20 caracteres"),

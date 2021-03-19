c069d7e58d3db3492a5f0bf79c083b102b5592fb
lucas1882 <lucas1882@gmail.com>
2021-03-19 14:24:46 -0300

    actualizacion de dashboard

--- dashboard/my-app/src/components/header/Header.jsx
+++ dashboard/my-app/src/components/header/Header.jsx
@@ -1,3 +1,5 @@
+import avatarImage from"../../assets/dummy-avatar.jpg"
+
 function Header() {
     return (
 
@@ -34,7 +36,7 @@ function Header() {
 						<li className="nav-item dropdown no-arrow">
 							<a className="nav-link dropdown-toggle" href="/" id="userDropdown">
 								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Walter White</span>
-								<img className="img-profile rounded-circle" src="assets/images/dummy-avatar.jpg" width="60" alt="avatar"/>
+								<img className="img-profile rounded-circle" src={avatarImage} width="60" alt="avatar"/>
 							</a>
 						</li>
 

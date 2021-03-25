2aaf2b4d873a8905d4803b4e80a6365a6474d85a
Juan Ignacio Guastello <jiguastello@hotmail.com>
2021-03-24 22:53:55 -0300

    api prettier

--- site/controllers/api/productsController.js
+++ site/controllers/api/productsController.js
@@ -1,45 +1,48 @@
-const { Product, Categoty, Sequelize, sequelize } = require("../../database/models");
+const {
+    Product,
+    Categoty,
+    Sequelize,
+    sequelize,
+} = require("../../database/models");
 const Op = Sequelize.Op;
 const { QueryTypes } = Sequelize;
-
-
 
 module.exports = {
     CountByCategory: (req, res) => {
         //→ objeto literal con una propiedad por categoría con el total de productos
-              Category.findAll({
-                   include: [{ association: "products" }],
-               }).then(function (category) {                                 
-                     res.send(category);
-               });
-
+        Category.findAll({
+            include: [{ association: "products" }],
+        }).then(function (category) {
+            for (let i = 0; i < category.length; i++) {
+                const countProducts = category[i].count;                
+            }
+            res.send(category);
+        });
     },
-    products: async (req,res) => {
-         const count = await Product.count();
-         Product.findAll({include:[{association:"category"}]})
-         .then(function (products) {
-            for (let i = 0; i < products.length; i++) {
-                products[i].setDataValue("detail","/api/product/" + products[i].id)
-              
-            }; 
-            let response = {
-                count: {count},
-                data:{ products
-            }};
+    products: async (req, res) => {
+        const count = await Product.count();
+        Product.findAll({ include: [{ association: "category" }] }).then(
+            function (products) {
+                for (let i = 0; i < products.length; i++) {
+                    products[i].setDataValue(
+                        "detail",
+                        "/api/product/" + products[i].id
+                    );
+                }
+                let response = {
+                    count: { count },
+                    data: { products },
+                };
 
-             res.send(response);
-         });
-
-
-
+                res.send(response);
+            }
+        );
     },
-    find: (req,res) => {
-    Product.findByPk(req.params.id, {
+    find: (req, res) => {
+        Product.findByPk(req.params.id, {
             include: [{ association: "category" }],
         }).then(function (product) {
             res.send(product);
         });
     },
-
 };
-
